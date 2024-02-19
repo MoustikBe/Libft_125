90a663b8116e3f4847082d5a572f4a6cab1f0ed0
Max <81183216+MoustikBe@users.noreply.github.com>
2024-02-19 21:48:52 +0100

    Add files via upload

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lsadd_back.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 15:42:08 by misaac-c          #+#    #+#             */
+/*   Updated: 2024/02/08 18:35:08 by misaac-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	temp = *lst;
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	while (temp -> next != NULL)
+		temp = temp -> next;
+	temp -> next = new;
+}
